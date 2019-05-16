bd890fd1d431d5788d92b72e1631e82a2500c9b2
Andrew Batz <abatz@wealthforge.com>
2019-05-16 15:30:47 -0400

    [iot] adding rope to the lockfile for refactoring, and initializing the direct_control and blink functions [#166063241]

--- stoplight.py
+++ stoplight.py
@@ -8,6 +8,8 @@ class Stoplight:
         self.gpio = gpio
         self.controller = {}
         self.fakeGpio = fakeGpio
+        self.ON = True
+        self.OFF = False
         # self.state = "null"
         for pinout in gpio:
             print("assigning " + pinout + " to GPIO pin: " + str(gpio[pinout]))
@@ -25,6 +27,28 @@ class Stoplight:
             if not self.fakeGpio:
                 sleep(2)
         self.assert_state("null")
+
+    def controll_pin(self, pin, instruction):
+        if instruction:
+            if not self.fakeGpio:
+                self.controller[pin].on()
+            else:
+                print("just turned on GPIO " + str(self.controller[pin]))
+        else:
+            if not self.fakeGpio:
+                self.controller[pin].off()
+            else:
+                print("just turned off GPIO " + str(self.controller[pin]))
+
+    def blink(self, pin, interval, duration):
+        oscilations = duration/(interval * 2)
+        for i in range(int(oscilations)):
+            print("pin ON")
+            self.controll_pin(pin, self.ON)
+            sleep(interval)
+            print("pin OFF")
+            self.controll_pin(pin, self.OFF)
+            sleep(interval)
 
     def assert_state(self, state):
         for key in self.states[state]:
